8b81931419c42714e43f05cb20965f045b197422
RuoChen95 <xrc_1995@163.com>
2017-01-17 12:03:24 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,9 +21,27 @@ function tipWorkTime() {
   workMsg.innerHTML = 'work must be at least 10 mins';
 }
 
-var work = document.getElementById('workTime');    
+var work = document.getElementById('workTime');
 var workMsg = document.getElementById('feedback'); 
 
 
 work.addEventListener('focus', tipWorkTime, false); 
 work.addEventListener('blur', checkWorkTime, false);
+
+// Create the HTML for the message
+var msg = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
+msg += '<div><h2>What is The Pomodoro Technique?</h2>';
+msg += 'For many people, time is an enemy. We race against the clock to finish assignments and meet deadlines. The Pomodoro Technique teaches you to work with time, instead of struggling against it. A revolutionary time management system, it is at once deceptively simple to learn and life-changing to use. ';
+msg += 'Essential to the Pomodoro Technique is the notion that taking short, scheduled breaks while working eliminates the running on fumes feeling you get when you have pushed yourself too hard. It is impossible to over work when you stick to the system. You may end up taking fewer sick days, too!</div>';
+
+var elNote = document.createElement('div');       // Create a new element
+elNote.setAttribute('id', 'note');                // Add an id of note
+elNote.innerHTML = msg;                           // Add the message
+document.body.appendChild(elNote);                // Add it to the page
+
+function dismissNote() {                          // Declare function
+  document.body.removeChild(elNote);              // Remove the note
+}
+
+var elClose = document.getElementById('close');   // Get the close button
+elClose.addEventListener('click', dismissNote, false);// Click close-clear note
